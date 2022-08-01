15cd5355d099d273026af474e35f2573fbd4f98b
Joaquin Armesto <89107170+JoaquinArm@users.noreply.github.com>
2022-08-01 12:39:10 -0300

    Add files via upload

---
+++ globals.js
@@ -0,0 +1,19 @@
+/*
+*   Details:
+*       Constant data needed to run one or more of the functions present in the project.
+*   Date:
+*       07-25-2022
+*   Author:
+*       Joaquin Armesto
+*/
+
+const PROJECTS =
+    {
+        'project_name':
+            {
+                'source_url': 'https://',
+                'objective_id': '',
+                'objective_sheet': '',
+                'objective_mirror': ''
+            }
+    }
